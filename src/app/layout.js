22fa170546fc788f8b8d539bce363257ae461466
John (Shiqi) Zhou <zhou9865@gmail.com>
2024-07-26 21:48:48 -0700

    Initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,22 @@
+/* eslint-disable new-cap */
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Firebase Next.js Hackpack",
+  description: "Firebase Setup Integration with Next.js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Toaster />
+        {children}
+      </body>
+    </html>
+  );
+}
